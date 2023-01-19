082f85d5a8dff7062741a7890b6d6e33a85cda6b
elektron1k <fop.holoborshchyi@gmail.com>
2023-01-19 19:21:08 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import GlobalFeed from 'pages/globalFeed';
+import Article from 'pages/article';
+import TopBar from 'components/topBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <TopBar />
+      <Routes>
+        <Route path="/" element={<GlobalFeed />} />
+        <Route path="/articles/:slug" element={<Article />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
